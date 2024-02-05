a915022f5cf816086c31dd1873ae592ccf348323
Lyee-Git <70620139+Lyee-Git@users.noreply.github.com>
2024-02-04 21:12:28 -0800

    SubExpression Interface Implemented

--- MileStone1/src/main/java/com/github/cse232b/subexpr/SubExpression.java
+++ MileStone1/src/main/java/com/github/cse232b/subexpr/SubExpression.java
@@ -1,5 +1,53 @@
 package com.github.cse232b.subexpr;
 
-public interface SubExpression {
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public interface SubExpression {
+    SubType getExprType();
+    List<Node> eval(List<Node> input) throws Exception;
+
+    /*
+        Get All descendants nodes if there is a double slash in subExpr
+     */
+    default void getDescendants(List<Node> input, List<Node> output) {
+        for (Node node : input) {
+            output.add(node);
+            NodeList children = node.getChildNodes();
+            List<Node> childrenNodes = new ArrayList<>();
+            for (int i = 0; i < children.getLength(); i++)
+                childrenNodes.add(children.item(i));
+            getDescendants(childrenNodes, output);
+        }
+    }
+
+    /*
+        An enumeration of all subexpression type
+     */
+    enum SubType {
+        SlashAp,
+        DslashAp,
+        SingleTagRp,
+        SingleStarRp,
+        SingleSelfRp,
+        SingleParentRp,
+        SingleTextRp,
+        SingleAttrRp,
+        SingleParenthRp,
+        BinarySlashRp,
+        BinaryDslashRp,
+        BinaryRp,
+        FilterRp,
+        RpF,
+        BinaryEqualRpF,
+        BinaryIsRpF,
+        RpEqualStringF,
+        ParenthF,
+        BinaryAndF,
+        BinaryOrF,
+        NegF
+    }
 }
